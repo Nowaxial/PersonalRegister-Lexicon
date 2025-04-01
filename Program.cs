105cd792587ee46b850f16cde1c6c5d1bb926b87
Maria Toledo <Nowaxial@users.noreply.github.com>
2025-04-01 16:21:40 +0200

    Second commit

--- Program.cs
+++ Program.cs
@@ -2,8 +2,83 @@
 
 class Program
 {
+
+    static List<Staff> personalRegister = new List<Staff>();
+
+    class Staff
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public int Salary { get; set; }
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        bool runningConsole = true;
+
+        while (runningConsole)
+        {
+            Console.WriteLine("Staff Register");
+            Console.WriteLine("1. Add an employee");
+            Console.WriteLine("2. Show all employees");
+            Console.WriteLine("3. Quit");
+            Console.Write("Choose an option: ");
+
+            string chosenValue = Console.ReadLine()!;
+
+            switch (chosenValue)
+            {
+                case "1":
+                    AddEmployee();
+                    break;
+
+                case "2":
+                    ShowAllEmployees();
+                    break;
+
+                case "3":
+                    runningConsole = false;
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice, try again!");
+                    break;
+            }
+        }
+    }
+
+
+    static void AddEmployee()
+    {
+        Staff employee = new Staff();
+        
+        Console.Write("Add first name: ");
+        employee.FirstName = Console.ReadLine()!;
+        
+        Console.Write("Add last name: ");
+        employee.LastName = Console.ReadLine()!;
+        
+        Console.Write("Add salary: ");
+        employee.Salary = int.Parse(Console.ReadLine()!);
+
+        personalRegister.Add(employee);
+        Console.WriteLine("Employee added!");
+    }
+
+    static void ShowAllEmployees()
+    {
+        if (personalRegister.Count == 0)
+        {
+            Console.WriteLine("No registered employees");
+            return;
+        }
+
+        Console.WriteLine("All employees:");
+        foreach (var employee in personalRegister)
+        {
+            Console.WriteLine("-------------------");
+            Console.WriteLine($"Name: {employee.FirstName} {employee.LastName}");
+            Console.WriteLine($"Salary: {employee.Salary} kr");
+            Console.WriteLine("-------------------");
+        }
     }
 }
